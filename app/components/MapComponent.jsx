3d60c9acb61093777cc5f28761e7a0c2ce124013
taotundex <taotundex001@gmail.com>
2024-05-28 20:08:23 +0100

    New commit

---
+++ app/components/MapComponent.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+// Fix for marker icons not showing
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
+const MapComponent = () => {
+  const position = [51.505, -0.09]; // Example position, replace with desired coordinates
+
+  return (
+    <MapContainer center={position} zoom={13} style={{ height: '100%', width: '100%' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={position}>
+        <Popup>
+          Location.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default MapComponent;
---
+++ app/components/SwiperComponent.tsx
@@ -0,0 +1,27 @@
+// import React from 'react';
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+// import { Navigation, Pagination } from 'swiper/modules';
+
+// const SwiperComponent = () => {
+//   return (
+//     <div>
+//         <Swiper
+//         modules={[Navigation, Pagination]}
+//         spaceBetween={50}
+//         slidesPerView={1}
+//         navigation
+//         pagination={{ clickable: true }}
+//         >
+//             <SwiperSlide><img src="/images/Group1.png" width='100%' className='rounded-xl size-full' alt="" /></SwiperSlide>
+//             <SwiperSlide><img src="/images/Group1.png" width='100%' className='rounded-xl size-full' alt="" /></SwiperSlide>
+//             <SwiperSlide><img src="/images/Group1.png" width='100%' className='rounded-xl size-full' alt="" /></SwiperSlide>
+//             <SwiperSlide><img src="/images/Group1.png" width='100%' className='rounded-xl size-full' alt="" /></SwiperSlide>
+//         </Swiper>
+//     </div>
+//   );
+// };
+
+// export default SwiperComponent;
